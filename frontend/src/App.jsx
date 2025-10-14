b3ba1f5d0d297b2165392e34d5ffc9083caee6e2
ddomir <irladominic@gmail.com>
2025-10-14 15:16:04 -0500

    Added Supabase Support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    getFilms();
+  }, []);
+
+  async function getFilms() {
+    const { data } = await supabase.from("Films").select();
+    setFilms(data);
+    console.log(data);
+  }
+
   return (
     <div>
-      <h1>Test</h1>
+      <h1 className="text-3xl font-bold underline">Database Films:</h1>
+      <ul>
+        {films.map((film) => (
+          <li key={film.name}>{film.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+    
+  );
 }
 
-export default App
+export default App;
